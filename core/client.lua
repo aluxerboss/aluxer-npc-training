a91d3bd36afc25a190d7b9bf5587356f2d006ac6
ALUXER <51151828+bosszalapee@users.noreply.github.com>
2021-12-12 08:59:31 +0700

    Update client.lua

--- core/client.lua
+++ core/client.lua
@@ -11,26 +11,26 @@ function SpawnNpcBoost()
     local playerCoords = GetEntityCoords(PlayerPedId())
     local v = Aluxer['about']['zone'][tonumber(location)]
     if v ~= nil then
-		local ped_model = string.upper(v.model)
-		RequestModel(GetHashKey(ped_model))
-		while not HasModelLoaded(GetHashKey(ped_model)) or not HasCollisionForModelLoaded(GetHashKey(ped_model)) do
-			Citizen.Wait(1)
-		end
-		local dist = v.dist or 20.0
-		local newX = v.coords.x + math.random(-dist, dist)
-		local newY = v.coords.y + math.random(-dist, dist)
-		local _, newZ = GetGroundZFor_3dCoord(newX+.0, newY+.0, v.coords.z+999.0, 1)
-		local ped = CreatePed(4, GetHashKey(ped_model), newX, newY, newZ, 0.0, false, false)
-		SetEntityHealth(ped, 300)
-		SetPedArmour(ped, 10000)
-		FreezeEntityPosition(ped, true)
-		SetBlockingOfNonTemporaryEvents(ped, true)
-		TaskStartScenarioInPlace(ped, "anim@heists@narcotics@funding@gang_idle", "gang_chatting_idle01", 0, false)
-		RequestAnimDict("anim@heists@narcotics@funding@gang_idle", "gang_chatting_idle01")
-		TaskPlayAnim(ped, "anim@heists@narcotics@funding@gang_idle", "gang_chatting_idle01", 20.0, -20.0, -1, 1, 0, false, false, false)
-		table.insert(npc_entity,{entity = ped})
-		num = num + 1
-		Citizen.Wait(1000)
+	local ped_model = string.upper(v.model)
+	RequestModel(GetHashKey(ped_model))
+	while not HasModelLoaded(GetHashKey(ped_model)) or not HasCollisionForModelLoaded(GetHashKey(ped_model)) do
+		Citizen.Wait(1)
+	end
+	local dist = v.dist or 20.0
+	local newX = v.coords.x + math.random(-dist, dist)
+	local newY = v.coords.y + math.random(-dist, dist)
+	local _, newZ = GetGroundZFor_3dCoord(newX+.0, newY+.0, v.coords.z+999.0, 1)
+	local ped = CreatePed(4, GetHashKey(ped_model), newX, newY, newZ, 0.0, false, false)
+	SetEntityHealth(ped, 300)
+	SetPedArmour(ped, 10000)
+	FreezeEntityPosition(ped, true)
+	SetBlockingOfNonTemporaryEvents(ped, true)
+	TaskStartScenarioInPlace(ped, "anim@heists@narcotics@funding@gang_idle", "gang_chatting_idle01", 0, false)
+	RequestAnimDict("anim@heists@narcotics@funding@gang_idle", "gang_chatting_idle01")
+	TaskPlayAnim(ped, "anim@heists@narcotics@funding@gang_idle", "gang_chatting_idle01", 20.0, -20.0, -1, 1, 0, false, false, false)
+	table.insert(npc_entity,{entity = ped})
+	num = num + 1
+	Citizen.Wait(1000)
     end
 end
 
@@ -48,15 +48,15 @@ end
 
 Citizen.CreateThread(function()
 	while true do
-		Citizen.Wait(0)
+	Citizen.Wait(0)
         local playerCoords = GetEntityCoords(PlayerPedId())
         location = GetLocationPoint()
         local v = Aluxer['about']['zone'][tonumber(location)]
         if v ~= nil and GetDistanceBetweenCoords(playerCoords, v.coords.x, v.coords.y, v.coords.z, true) < v.dist then
-			if #npc_entity < v.max then
-				SpawnNpcBoost()
-			end
-			Citizen.Wait(v.respawn)
+		if #npc_entity < v.max then
+			SpawnNpcBoost()
+		end
+		Citizen.Wait(v.respawn)
         else
             Citizen.Wait(500)
         end
